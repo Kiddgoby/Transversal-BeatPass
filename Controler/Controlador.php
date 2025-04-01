63a947c341bb0c035690cd5066535e791c4c337c
Javier G. <xavichavochavuski@gmail.com>
2025-04-01 09:51:41 +0200

    Supuesto controlador 2

--- Controler/Controlador.php
+++ Controler/Controlador.php
@@ -1,30 +1,58 @@
-<?phprequire_once 'modelo/Usuario.php';
+<?php
+ 
+ session_start();
+ 
+    if ($_SERVER["REQUEST_METHOD"]== "POST"){
+        $user = new UserController();
 
-class Controlador {
+        if (isset($_POST["Login"])) {
+            $gmail = $_POST["gmail"];
+            $password = $_POST["password"];
     
-    public function procesarLogin() {
-        $mensaje = "";
-        $acceso = false;
-        
-        
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $correo = isset($_POST['gmail']) ? $_POST['gmail'] : '';
-            $password = isset($_POST['password']) ? $_POST['password'] : '';
-            
-        
-            $usuario = new Usuario($correo, $password);
-            
-        if ($usuario->validarCredenciales()){
+            if ($user->login($gmail, $password)) {
+                echo "<p>Inicio de sesión exitoso.</p>";
+            } else {
+                echo "<p>Correo o contraseña incorrectos.</p>";
+            }
+
+        if (isset($_Post["Logout"])){
+            echo "<p> Logout Button is clicked.</p>";
+            $user->logout();
+        }
+
+        if (isset($_Post["Register"])){
+            echo "<p> Regiter Button is clicked.</p>";
+            $user->register();
+        }
+
+    }
+
+    Class UserController
+    {
+        private $conn;
+        public function login($gmail, $password)
+        {
+            // Datos de acceso simulados
+            $userGmail = "abcd@gmail.com";
+            $userPassword = "1234";
+    
+            if ($gmail == $userGmail && $password == $userPassword) {
+                return true;
+            } else {
+                return false;
+            }
+
+    }
+
+    public function logout(){
 
 
-            $acceso = true;
-            $mensaje = "¡Acceso conedido!";
+    }
 
-            session_start();
-            $_SESSION['usuario']=$correo;
-            $_SESSION['autenticado']= true;
+    public function register(){
 
-        }    
-
-
+    }
 }
+    }
+
+    
